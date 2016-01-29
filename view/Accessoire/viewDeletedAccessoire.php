e76293976b71387b8bb071be767d27493e31a24f
Harkame <louisr.daviaud@gmail.com>
2016-01-29 16:18:19 +0100

    Create viewDeletedAccessoire.php

---
+++ view/Accessoire/viewDeletedAccessoire.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+
+<html>
+    <?php include ("{$ROOT}{$DS}view{$DS}Generic{$DS}Header.php") ?>
+    <?php include ("{$ROOT}{$DS}view{$DS}Generic{$DS}Menu.php") ?>
+        <?php
+          echo '<p> L\'accessoire a été supprimé.</p>';
+        ?>
+    <?php include ("{$ROOT}{$DS}view{$DS}Generic{$DS}Footer.php") ?>
+</html>
