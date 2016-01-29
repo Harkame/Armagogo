5e9962f9d4c556af0e160f792743691113512e31
Harkame <louisr.daviaud@gmail.com>
2016-01-29 16:20:29 +0100

    Create viewDeletedArme.php

---
+++ view/Arme/viewDeletedArme.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+
+<html>
+    <?php include ("{$ROOT}{$DS}view{$DS}Generic{$DS}Header.php") ?>
+    <?php include ("{$ROOT}{$DS}view{$DS}Generic{$DS}Menu.php") ?>
+        <?php
+        echo '<p>L\'arme a été supprimé de la base.</p>';
+        ?>
+    <?php include ("{$ROOT}{$DS}view{$DS}Generic{$DS}Footer.php") ?>
+</html>
