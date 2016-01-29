2f5c4e61bfcc8dd77c699aec576de5177aa0d21e
Harkame <louisr.daviaud@gmail.com>
2016-01-29 16:27:38 +0100

    Create viewPasCo.php

---
+++ view/Utilisateur/viewPasCo.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+
+<html>
+    <?php include ("{$ROOT}{$DS}view{$DS}Generic{$DS}Header.php") ?>
+    <?php include ("{$ROOT}{$DS}view{$DS}Generic{$DS}Menu.php") ?>
+        <?php
+			echo 'Veuillez vous connecter';
+        ?>
+    <?php include ("{$ROOT}{$DS}view{$DS}Generic{$DS}Footer.php") ?>
+</html>
