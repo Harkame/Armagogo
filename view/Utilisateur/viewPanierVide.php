6138b3c6616cc3bcaacb8096c0f97cc25c13df09
Harkame <louisr.daviaud@gmail.com>
2016-01-29 16:27:19 +0100

    Create viewPanierVide.php

---
+++ view/Utilisateur/viewPanierVide.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+
+<html>
+    <?php include ("{$ROOT}{$DS}view{$DS}Generic{$DS}Header.php") ?>
+    <?php include ("{$ROOT}{$DS}view{$DS}Generic{$DS}Menu.php") ?>
+        <?php    
+			echo 'Panier vide';
+        ?>
+    <?php include ("{$ROOT}{$DS}view{$DS}Generic{$DS}Footer.php") ?>
+</html>
