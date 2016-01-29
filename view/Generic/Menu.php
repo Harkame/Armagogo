d1a27fcd1eb52560bba92041e2f2d18ef0ea9490
Harkame <louisr.daviaud@gmail.com>
2016-01-29 16:21:32 +0100

    Create Menu.php

---
+++ view/Generic/Menu.php
@@ -0,0 +1,5 @@
+<nav>
+<a href="http://infolimon.iutmontp.univ-montp2.fr//~massebiauv/projet/index.php?menu=armes"> Armes </a>;
+<a href="http://infolimon.iutmontp.univ-montp2.fr//~massebiauv/projet/index.php?menu=munitions"> Munitions </a>;
+<a href="http://infolimon.iutmontp.univ-montp2.fr//~massebiauv/projet/index.php?menu=accessoires"> Accessoires </a>;
+</nav>
