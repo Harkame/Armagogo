791c3ccdb58bfedc8cdacc442be822e46f9c67d4
Harkame <louisr.daviaud@gmail.com>
2016-01-29 16:21:01 +0100

    Create Accueil.php

---
+++ view/Generic/Accueil.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+
+<html>
+	<?php include ("{$ROOT}{$DS}view{$DS}Generic{$DS}Header.php") ?>
+   <?php include ("{$ROOT}{$DS}view{$DS}Generic{$DS}Menu.php") ?>
+   <?php echo 'Bienvenue sur notre site !!'?>
+<?php include ("{$ROOT}{$DS}view{$DS}Generic{$DS}Footer.php") ?>
+</html>
