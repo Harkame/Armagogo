d68adf0d886b0a17ca043ea3c530a114d6c13f00
Harkame <louisr.daviaud@gmail.com>
2016-01-29 16:22:45 +0100

    Create viewDeletedMunitions.php

---
+++ view/Munitions/viewDeletedMunitions.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+
+<html>
+    <?php include ("{$ROOT}{$DS}view{$DS}Generic{$DS}Header.php") ?>
+    <?php include ("{$ROOT}{$DS}view{$DS}Generic{$DS}Menu.php") ?>
+        <?php
+          echo '<p> Les munitions ont été supprimés.</p>';
+        ?>
+    <?php include ("{$ROOT}{$DS}view{$DS}Generic{$DS}Footer.php") ?>
+</html>
