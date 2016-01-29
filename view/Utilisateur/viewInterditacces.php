5c4952bdb4d35f5318337b84a5b0b6c241a856ce
Harkame <louisr.daviaud@gmail.com>
2016-01-29 16:25:37 +0100

    Create viewInterditacces.php

---
+++ view/Utilisateur/viewInterditacces.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<?php include ("{$ROOT}{$DS}view{$DS}Generic{$DS}Header.php") ?>
+    <?php include ("{$ROOT}{$DS}view{$DS}Generic{$DS}Menu.php") ?>
+			<fieldset>
+				<?php
+					echo "Alors ? On est en P.L.S ? ";
+?>
+			</fieldset>
+		</form>
+		<p> <a href="index.php"> Retour au menu principal </a></p>
+    <?php include ("{$ROOT}{$DS}view{$DS}Generic{$DS}Footer.php") ?>
+</html>
