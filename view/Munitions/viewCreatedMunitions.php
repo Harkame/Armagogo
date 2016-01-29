eb39f2ebf30ec5909186ed6a609328c800584d40
Harkame <louisr.daviaud@gmail.com>
2016-01-29 16:22:17 +0100

    Create viewCreatedMunitions.php

---
+++ view/Munitions/viewCreatedMunitions.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+
+<html>
+    <?php include ("{$ROOT}{$DS}view{$DS}Generic{$DS}Header.php") ?>
+    <?php include ("{$ROOT}{$DS}view{$DS}Generic{$DS}Menu.php") ?>
+        <?php
+		echo '<p>Les munitions '.$nom.' a été ajouté à la base.</p>'
+        ?>
+    <?php include ("{$ROOT}{$DS}view{$DS}Generic{$DS}Footer.php") ?>
+</html>
