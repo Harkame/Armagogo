155c2755f54a1a862b27f25b33869ff0c32afb09
Harkame <louisr.daviaud@gmail.com>
2016-01-29 16:24:48 +0100

    Create viewDeconnecter.php

---
+++ view/Utilisateur/viewDeconnecter.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+	<?php include ("{$ROOT}{$DS}view{$DS}Generic{$DS}Header.php") ?>
+    <?php include ("{$ROOT}{$DS}view{$DS}Generic{$DS}Menu.php") ?>
+		<p> Vous êtes deconnecté </p>
+		<p> <a href="index.php"> Retour au menu principal </a></p>
+    <?php include ("{$ROOT}{$DS}view{$DS}Generic{$DS}Footer.php") ?>
+</html>
